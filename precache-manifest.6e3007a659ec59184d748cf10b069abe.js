250e288cbf9debc8a1ed16ef1fa908bb5e9997ba
kamilmoskal <kamil_moskal@interia.pl>
2019-01-17 16:14:05 +0100

    Updates

---
+++ precache-manifest.6e3007a659ec59184d748cf10b069abe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c2016388749b9af5d6",
+    "url": "/react-simple-livechat/static/js/runtime~main.84c20163.js"
+  },
+  {
+    "revision": "797e0a8ea99e5a435b9e",
+    "url": "/react-simple-livechat/static/js/main.797e0a8e.chunk.js"
+  },
+  {
+    "revision": "44e1dcd1a45245a770ef",
+    "url": "/react-simple-livechat/static/js/1.44e1dcd1.chunk.js"
+  },
+  {
+    "revision": "797e0a8ea99e5a435b9e",
+    "url": "/react-simple-livechat/static/css/main.9c738a6a.chunk.css"
+  },
+  {
+    "revision": "44e1dcd1a45245a770ef",
+    "url": "/react-simple-livechat/static/css/1.217a140b.chunk.css"
+  },
+  {
+    "revision": "c0c3cb9b7199c87b8a4978b844744ed5",
+    "url": "/react-simple-livechat/index.html"
+  }
+];
